e18fcb9df350d765c20fd4df04980e006183115e
Nikhar\Nikhar_Pandya <pandyanikhar@gmail.com>
2020-12-02 15:57:47 -0500

    push changes

---
+++ JavaScript/Script.js
@@ -0,0 +1,18 @@
+class Calculator {
+	constructor
+}
+
+
+
+
+
+
+
+const numberButtons = document.querySelectorALL('[data-number]')
+const operationButtons = document.querySelectorALL('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const perviousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
